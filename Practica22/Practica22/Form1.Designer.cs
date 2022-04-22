36a8bf5816d23eaba600d775fa40e78039583d8d
сергей лебёдкин <sergeqlebedkin1234@gmail.com>
2022-04-22 21:05:08 +0300

    First commit

---
+++ Practica22/Practica22/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Practica22
+{
+    partial class fAutoriz
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pLogo = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lZag = new System.Windows.Forms.Label();
+            this.lLogin = new System.Windows.Forms.Label();
+            this.tbLogin = new System.Windows.Forms.TextBox();
+            this.tbPass = new System.Windows.Forms.TextBox();
+            this.lPass = new System.Windows.Forms.Label();
+            this.llReg = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnAutor = new System.Windows.Forms.Button();
+            this.pLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pLogo
+            // 
+            this.pLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(225)))));
+            this.pLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pLogo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pLogo.Controls.Add(this.lZag);
+            this.pLogo.Controls.Add(this.pictureBox1);
+            this.pLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pLogo.ForeColor = System.Drawing.Color.Black;
+            this.pLogo.Location = new System.Drawing.Point(0, 0);
+            this.pLogo.Name = "pLogo";
+            this.pLogo.Size = new System.Drawing.Size(530, 102);
+            this.pLogo.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pictureBox1.Image = global::Practica22.Properties.Resources.fabric_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(428, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lZag
+            // 
+            this.lZag.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lZag.AutoSize = true;
+            this.lZag.Font = new System.Drawing.Font("Tw Cen MT", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lZag.Location = new System.Drawing.Point(148, 31);
+            this.lZag.Name = "lZag";
+            this.lZag.Size = new System.Drawing.Size(178, 31);
+            this.lZag.TabIndex = 1;
+            this.lZag.Text = "Авторизация";
+            // 
+            // lLogin
+            // 
+            this.lLogin.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lLogin.AutoSize = true;
+            this.lLogin.Font = new System.Drawing.Font("Tw Cen MT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lLogin.Location = new System.Drawing.Point(77, 138);
+            this.lLogin.Name = "lLogin";
+            this.lLogin.Size = new System.Drawing.Size(56, 24);
+            this.lLogin.TabIndex = 1;
+            this.lLogin.Text = "Логин";
+            // 
+            // tbLogin
+            // 
+            this.tbLogin.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tbLogin.Font = new System.Drawing.Font("Tw Cen MT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbLogin.Location = new System.Drawing.Point(81, 165);
+            this.tbLogin.Name = "tbLogin";
+            this.tbLogin.Size = new System.Drawing.Size(360, 30);
+            this.tbLogin.TabIndex = 2;
+            // 
+            // tbPass
+            // 
+            this.tbPass.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tbPass.Font = new System.Drawing.Font("Tw Cen MT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPass.Location = new System.Drawing.Point(81, 241);
+            this.tbPass.Name = "tbPass";
+            this.tbPass.PasswordChar = '*';
+            this.tbPass.Size = new System.Drawing.Size(360, 30);
+            this.tbPass.TabIndex = 4;
+            // 
+            // lPass
+            // 
+            this.lPass.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lPass.AutoSize = true;
+            this.lPass.Font = new System.Drawing.Font("Tw Cen MT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lPass.Location = new System.Drawing.Point(77, 214);
+            this.lPass.Name = "lPass";
+            this.lPass.Size = new System.Drawing.Size(68, 24);
+            this.lPass.TabIndex = 3;
+            this.lPass.Text = "Пароль";
+            // 
+            // llReg
+            // 
+            this.llReg.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.llReg.AutoSize = true;
+            this.llReg.Font = new System.Drawing.Font("Tw Cen MT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.llReg.Location = new System.Drawing.Point(334, 274);
+            this.llReg.Name = "llReg";
+            this.llReg.Size = new System.Drawing.Size(107, 24);
+            this.llReg.TabIndex = 5;
+            this.llReg.TabStop = true;
+            this.llReg.Text = "Регистрация";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(156)))), ((int)(((byte)(26)))));
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Viner Hand ITC", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(196, 316);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 67);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "teex";
+            // 
+            // BtnAutor
+            // 
+            this.BtnAutor.AutoSize = true;
+            this.BtnAutor.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BtnAutor.Font = new System.Drawing.Font("Tw Cen MT", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAutor.Location = new System.Drawing.Point(0, 434);
+            this.BtnAutor.Name = "BtnAutor";
+            this.BtnAutor.Size = new System.Drawing.Size(530, 54);
+            this.BtnAutor.TabIndex = 7;
+            this.BtnAutor.Text = "Авторизация";
+            this.BtnAutor.UseVisualStyleBackColor = true;
+            // 
+            // fAutoriz
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 488);
+            this.Controls.Add(this.BtnAutor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.llReg);
+            this.Controls.Add(this.tbPass);
+            this.Controls.Add(this.lPass);
+            this.Controls.Add(this.tbLogin);
+            this.Controls.Add(this.lLogin);
+            this.Controls.Add(this.pLogo);
+            this.Font = new System.Drawing.Font("Tw Cen MT", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "fAutoriz";
+            this.Text = "Авторизация";
+            this.pLogo.ResumeLayout(false);
+            this.pLogo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pLogo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lZag;
+        private System.Windows.Forms.Button BtnAutor;
+        public System.Windows.Forms.Label lLogin;
+        public System.Windows.Forms.TextBox tbLogin;
+        public System.Windows.Forms.TextBox tbPass;
+        public System.Windows.Forms.Label lPass;
+        public System.Windows.Forms.LinkLabel llReg;
+        public System.Windows.Forms.Label label1;
+    }
+}
+
